2b01f6ef606b4582b27dfd6ae1ee7748ccbe1cc8
Matt Cousineau <mjcousineau1@gmail.com>
2022-02-08 14:38:56 -0500

    Changed to mui design and added test header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
 import "../styles/globals.css";
-import "antd/dist/antd.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import CssBaseline from "@mui/material/CssBaseline";
+
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
-import { Breadcrumb, Layout, Menu } from "antd";
-import { Content, Footer, Header } from "antd/lib/layout/layout";
 import type { NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
+import ResponsiveAppBar from "../components/MainAppBar";
 
 const Home: NextPage = () => {
   return (
@@ -10,8 +8,10 @@ const Home: NextPage = () => {
       <Head>
         <title>TASK TASK</title>
         <meta name="description" content="Task Task Work Request System" />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <ResponsiveAppBar />
     </div>
   );
 };
