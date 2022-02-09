13d923b2e0176ea3198afd611773be08551b7155
Matt Cousineau <mjcousineau1@gmail.com>
2022-02-08 19:03:50 -0500

    fixed some header problems

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,18 +4,34 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import CssBaseline from "@mui/material/CssBaseline";
-
+import createEmotionCache from "../src/createEmotionCache";
 import type { AppProps } from "next/app";
 import Header from "../components/MainHeader";
+import theme from "../src/theme";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <CssBaseline />
-      <Header />
-      <Component {...pageProps} />
-    </>
-  );
+import { CacheProvider, EmotionCache, ThemeProvider } from "@emotion/react";
+import Head from "next/head";
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
 }
 
-export default MyApp;
+export default function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Header />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
