316e471c288491639de2f46d7f67943f6cdfecb8
Matt Cousineau <mjcousineau1@gmail.com>
2022-02-09 20:14:20 -0500

    naming fixes before things get too big

---
+++ pages/work-requests.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import EnhancedTable from "../components/WorkRequestTable";
+
+const WorkRequests: NextPage = () => {
+  return (
+    <div>
+      <EnhancedTable />
+    </div>
+  );
+};
+
+export default WorkRequests;
